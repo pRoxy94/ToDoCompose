87a88647b7b1ba4489d22d436bcd69fcd2eab625
Rossana Laricchia <rossana.laricchia@gmail.com>
2022-03-09 21:25:51 +0100

    Setup Dagger-Hilt

---
+++ app/src/main/java/it/proxy/todocompose/ToDoApplication.kt
@@ -0,0 +1,9 @@
+package it.proxy.todocompose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ToDoApplication: Application() {
+
+}
---
+++ app/src/main/java/it/proxy/todocompose/di/DatabaseModule.kt
@@ -0,0 +1,31 @@
+package it.proxy.todocompose.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import it.proxy.todocompose.data.ToDoDatabase
+import it.proxy.todocompose.util.Constants.DATABASE_NAME
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        ToDoDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: ToDoDatabase) = database.toDoDao()
+}
